275b07fe13199ed43c972c4b59043d2a919e77cc
Lukas Schönsgibl <lukas@schoensgibl.com>
2019-11-21 04:51:11 +0100

    secure user setting

--- events/forms.py
+++ events/forms.py
@@ -1,7 +1,16 @@
+import datetime
+from datetime import timedelta
 from django import forms
+from django.forms import ModelChoiceField
+from django.forms import DateTimeField
+
+from django.contrib.admin.widgets import AdminDateWidget
 
 class EventForm(forms.Form):
+    INPUT_FORMATS = ['%Y-%m-%d %H:%M']
+    INPUT_FORMATS_DAY = ['%Y-%m-%d']
+    INPUT_FORMATS_TIME = ['%H:%M']
     name = forms.CharField(max_length=50, required=True)
     venue = forms.CharField(max_length=30, required=False)
-    date = forms.DateField(input_formats=['%Y-%m-%d'], help_text="JJJJ-MM-DD")
-    time = forms.TimeField(widget=forms.TimeInput(format='%H:%M'), help_text="hh:mm")
+    date = forms.DateField(widget=forms.TextInput(attrs={'class':'datepicker'}))
+    time = forms.TimeField(input_formats=INPUT_FORMATS_TIME, widget=forms.DateTimeInput())
