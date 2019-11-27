2079e053a6b43afac9d396cb12ce770eadc6ea24
Lukas Schönsgibl <lukas@schoensgibl.com>
2019-11-27 08:56:10 +0100

    change password

--- users/views.py
+++ users/views.py
@@ -10,7 +10,25 @@ from events.forms import EventForm
 from .forms import CustomUserCreationForm
 from django.utils.datastructures import MultiValueDictKeyError
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'registration/password_change.html', {
+        'form': form
+    })
 
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
