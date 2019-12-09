350a09f4b99b367fbc224c2ff919e1e028d64ff5
Lukas Schönsgibl <lukas@schoensgibl.com>
2019-12-09 07:49:37 +0100

    urls to german

--- students/urls.py
+++ students/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signin/', views.signInView, name='students_signin'),
-    path('signin/success/', views.successView, name='student_successfully_saved'),
+    path('anmeldung/', views.signInView, name='students_signin'),
+    path('anmeldung/erfolgreich/', views.successView, name='student_successfully_saved'),
 ]
--- teaching/urls.py
+++ teaching/urls.py
@@ -10,7 +10,7 @@ from teaching.show_teacher_view import show_teacher_view
 urlpatterns = [
     #path('allstudents', teaching.get_students.get_all_students, name='get_all_students'),
     #path('singlestudent', teaching.get_students.get_student, name='get_student'),
-    path('unsere-paedagog_innen', teaching.show_teacher_view.show_teacher_view, name="all_teachers"),
+    path('ueber-uns', teaching.show_teacher_view.show_teacher_view, name="all_teachers"),
     path('bildungsangebot-musikschule', teaching.views.teaching_music_view, name="teaching_music"),
     path('bildungsangebot-kunstschule', teaching.views.teaching_art_view, name="teaching_art"),
     path('beitraege-ermaessigungen', teaching.views.teaching_prices_view, name="teaching_prices"),
