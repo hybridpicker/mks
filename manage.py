2847d52114451864fbbd35adbcc91b11912c6a5b
Lukas Schönsgibl <lukas@schoensgibl.com>
2020-01-29 12:36:39 +0100

    reset manage.py

--- manage.py
+++ manage.py
@@ -15,13 +15,13 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    
-    if len(sys.argv) == 2 and sys.argv[1] == 'migrate':
-        execute_from_command_line(['manage.py', 'import_data'])
 
 """
 Combine migrate with fixture Loading
 """
+#    if len(sys.argv) == 2 and sys.argv[1] == 'migrate':
+#        execute_from_command_line(['manage.py', 'import_data'])
+
 
 
 if __name__ == '__main__':
