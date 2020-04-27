4bdda3eaef719f64d162d85ff2c544061632c5bb
Lukas Schönsgibl <lukas@schoensgibl.com>
2020-04-27 08:29:51 +0200

    index form view

---
+++ controlling/forms.py
@@ -0,0 +1,10 @@
+from ckeditor.widgets import CKEditorWidget
+from django.utils.translation import gettext as _
+from django import forms
+from home.models import IndexText
+
+class IndexForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+    class Meta:
+        model = IndexText
+        fields = ['content']
