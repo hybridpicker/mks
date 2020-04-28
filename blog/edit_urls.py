4503b32fea3f7b62a81bcb3b58e2ec94b0f9b3b6
Lukas Schönsgibl <lukas@schoensgibl.com>
2020-04-28 15:19:56 +0200

    edit blog posts

--- blog/edit_urls.py
+++ blog/edit_urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post/<id>', views.BlogPostEditView.as_view(), name='blog_post_edit_detail'),
+    path('post/<int:pk>/edit/', views.post_edit, name='blog_post_edit_detail'),
     path('new', views.create_blog, name='create_blog_post'),
     path('summary', views.show_blogs_editing, name='show_blogs_editing'),
 ]
