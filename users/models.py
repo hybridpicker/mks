956101c5d0949d49792a2ed1c746d29047aa6aa2
hybridpicker <lukas@schoensgibl.com>
2022-05-14 18:12:36 +0200

    customuser coordinator

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     first_name = models.CharField(_(u'First Name'), max_length=30)
     last_name = models.CharField(_(u'Last Name'), max_length=30)
+    coordinator = models.BooleanField(default=False)
 
     def __str__(self):
         return '%s %s' % (self.first_name, self.last_name)
