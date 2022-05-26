728c852b333d461f4a8b31147c381bfe46d2c7cc
hybridpicker <lukas@schoensgibl.com>
2022-05-26 15:11:58 +0200

    new student create view

--- controlling/urls.py
+++ controlling/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('students', views.get_all_students, name='get_controlling_students'),
     path('coordinator/students', views.get_all_students_coordinator, name='get_controlling_students_coordinator'),
     path('single_student', views.get_student, name='get_controlling_single_student'),
+    path('new_student', views.newStudentView, name='create_new_student'),
     path('parent', views.get_parent, name='get_controlling_parent'),
     path('coordinator/single_student', views.get_student_coordinator, name='get_controlling_single_student_coordinator'),
-    path('index_text', views.get_index_text, name='get_index_text')
+    path('index_text', views.get_index_text, name='get_index_text'),
+    path('new_student/sucess', views.newStudentSuccessView, name='new_student_saved'),
 ]
--- controlling/views.py
+++ controlling/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
+import datetime
 
 from django.shortcuts import render, redirect
 
@@ -9,6 +10,7 @@ from home.models import IndexText
 from controlling.forms import IndexForm, SingleStudentDataForm
 from controlling.forms import SingleStudentDataFormCoordinator, ParentDataForm
 from teaching.models import Teacher
+from students.forms import SignInForm
 
 from django.utils.datastructures import MultiValueDictKeyError
 
@@ -167,3 +169,70 @@ def get_index_text(request):
         'form': form,
         }
     return render(request, 'controlling/index_form.html', context)
+
+@login_required(login_url='/team/login/')
+@staff_member_required
+def newStudentView(request):
+    form = SignInForm(request.POST)
+    if request.method == 'POST':
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            '''
+            Clean data
+            '''
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            from_email = form.cleaned_data['from_email']
+            subject = form.cleaned_data['subject']
+            adress_line = form.cleaned_data['adress_line']
+            house_number = form.cleaned_data['house_number']
+            postal_code = form.cleaned_data['postal_code']
+            city = form.cleaned_data['city']
+            email = form.cleaned_data['from_email']
+            parent_first_name = request.POST['parent_first_name']
+            parent_last_name = request.POST['parent_last_name']
+            phone = request.POST['phone']
+            '''
+            Request Data
+            '''
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            parent_first_name = request.POST['parent_first_name']
+            parent_last_name = request.POST['parent_last_name']
+            from_email = request.POST['from_email']
+            birthdate_year = request.POST['birthdate_year']
+            birthdate_month = request.POST['birthdate_month']
+            birthdate_day = request.POST['birthdate_day']
+            adress_line = request.POST['adress_line']
+            subject = form.cleaned_data['subject'].id
+            house_number = request.POST['house_number']
+            postal_code = request.POST['postal_code']
+            city = request.POST['city']
+            email = request.POST['from_email']
+            phone = request.POST['phone']
+            date_string = birthdate_month + ' ' + birthdate_day + ' ' + birthdate_year
+            birthdate = datetime.datetime.strptime(date_string, '%m %d %Y')
+            new_parent = Parent(first_name=parent_first_name,
+                                last_name=parent_last_name,
+                                house_number=house_number,
+                                postal_code=postal_code,
+                                adress_line=adress_line,
+                                city=city,
+                                email=email,
+                                phone=phone)
+            new_parent.save()
+            new_student = Student(first_name=first_name,
+                                  last_name=last_name,
+                                  birth_date=birthdate,
+                                  subject_id=subject,
+                                  parent_id=new_parent.id)
+            new_student.save()
+            return redirect('new_student_saved')
+    else:
+        form = SignInForm()
+    return render(request, "controlling/new_student.html", {'form':form})
+
+@login_required(login_url='/team/login/')
+@staff_member_required
+def newStudentSuccessView(request):
+    return render(request, "controlling/new_student_success.html")
