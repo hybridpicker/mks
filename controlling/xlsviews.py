00c07a9ab5174f1db064edf63b7e834d9b10db4e
hybridpicker <lukas@schoensgibl.com>
2022-05-29 20:48:16 +0200

    export xls

--- controlling/xlsviews.py
+++ controlling/xlsviews.py
@@ -4,12 +4,18 @@ import datetime
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
-from students.models import Student, Parent
+from students.models import Student
+from teaching.models import SubjectCategory
 
 # Create your views here.
 @login_required(login_url='/team/login/')
 @staff_member_required
 def export_students_xls(request):
+    try:
+        category_id = request.GET['id']
+        category = SubjectCategory.objects.get(id=category_id)
+    except:
+        category = None
     response = HttpResponse(content_type='application/ms-excel')
     response['Content-Disposition'] = 'attachment; filename="students.xls"'
 
@@ -34,11 +40,14 @@ def export_students_xls(request):
     font_style = xlwt.XFStyle()
     style1 = xlwt.easyxf(num_format_str='D-MMM-YY')
     font_style.num_format_str = 'dd/mm/yyyy'
-
-    rows = Student.objects.all().values_list('start_date', 'parent__first_name', 'parent__last_name', 'first_name', 
-                                            'last_name', 'parent__email', 'birth_date', 'subject__subject',
-                                            'parent__adress_line', 'parent__house_number', 'parent__postal_code', 
-                                            'parent__city', 'parent__phone', 'note')
+    if category:
+        students = Student.objects.filter(subject__category=category_id)
+    else:
+        students = Student.objects.all()
+    rows = students.values_list('start_date', 'parent__first_name', 'parent__last_name', 'first_name', 
+                                'last_name', 'parent__email', 'birth_date', 'subject__subject',
+                                'parent__adress_line', 'parent__house_number', 'parent__postal_code', 
+                                'parent__city', 'parent__phone', 'note')
     for row in rows:
         row_num += 1
         for col_num in range(len(row)):
