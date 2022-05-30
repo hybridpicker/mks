e2295777b1b3d9769a4666d0b949275c22c3573a
hybridpicker <lukas@schoensgibl.com>
2022-05-30 14:25:13 +0200

    fix parent xls

--- controlling/xlsviews.py
+++ controlling/xlsviews.py
@@ -50,7 +50,7 @@ def export_students_xls(request):
     rows = students.values_list('start_date', 'parent__first_name', 'parent__last_name', 'first_name', 
                                 'last_name', 'parent__email', 'birth_date', 'subject__subject',
                                 'parent__adress_line', 'parent__house_number', 'parent__postal_code', 
-                                'parent__city', 'parent__phone', 'teacher__first_name', 'teacher__lastname',
+                                'parent__city', 'parent__phone', 'teacher__first_name', 'teacher__last_name',
                                 'note')
     for row in rows:
         row_num += 1
